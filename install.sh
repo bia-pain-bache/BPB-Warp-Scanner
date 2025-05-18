d5f4d9877fea313b0fd4d3555eea1103014945cf
bia-pain-bache <155004885+bia-pain-bache@users.noreply.github.com>
2025-05-18 15:08:21 +0330

    Added installer

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+OS=$(uname -s)
+if [ "$OS" != "Linux" ]; then
+    echo "This script only supports Linux/Android platforms."
+    exit 1
+fi
+
+ARCH=$(uname -m)
+case "$ARCH" in
+    aarch64|arm64) ARCH="arm64" ;;
+    armv7*|armv8*) ARCH="arm" ;;
+    x86_64)        ARCH="amd64" ;;
+    i386|i686)     ARCH="386" ;;
+    *)             echo "Unsupported architecture: $ARCH" && exit 1 ;;
+esac
+
+rm -f BPB-Warp-Scanner.tar.gz BPB-Warp-Scanner-linux-*
+
+BINARY="BPB-Warp-Scanner-linux-${ARCH}"
+ARCHIVE="${BINARY}.tar.gz"
+
+echo "Downloading ${ARCHIVE}..."
+curl -L -# -o "$ARCHIVE" "https://github.com/bia-pain-bache/BPB-Warp-Scanner/releases/latest/download/${ARCHIVE}" && \
+tar xzf "$ARCHIVE" && \
+chmod +x "./${BINARY}" && \
+exec ./"${BINARY}"
